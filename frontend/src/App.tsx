67aa1eb69c48cb5efd48e54ce43585f0b32821bf
Dawsonkw <dawsonkw@gmail.com>
2024-10-27 14:04:07 -0600

    Initial push of FindAFight, the app that lets you order a round of fisticuffs, right to your very own front door! It's like uber for fighting. Seriously though once again this is parody. This push is so I dont forget the project which keeps happening when I make a blank folder on my pc with just the project name and somehow future me is supposed to decipher the point. WHAT DOES MONTHLYSUBTRACKER EVEN MEAN DAWSON?

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>ITS LIKE UBER, BUT FOR FIGHTING.</h1>
+      <p>
+        Have you ever wanted to fight your uber driver? Skip the stress of
+        ordering a ride and just order yourself a round of fisticuffs
+      </p>
+      <p>Do I really need to state that this is parody again?</p>
+    </>
+  );
+}
+
+export default App;
